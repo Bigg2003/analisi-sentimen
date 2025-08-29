9dd4f2ca1b3ae5e29f5a3ae6ab9f01e558fa54ef
Bigg2003 <dwifajar.2024@student.uny.ac.id>
2025-08-29 21:15:43 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,25 @@
+import joblib
+import streamlit as st
+
+# Load model & vectorizer
+model = joblib.load("C:\\Users\\Thinkpad T480\\Desktop\\Hackthon\\model\\model_sentiment.joblib")
+vectorizer = joblib.load("C:\\Users\\Thinkpad T480\\Desktop\\Hackthon\\model\\vectorizer.joblib")
+
+# Streamlit app
+st.title("Analisis Sentimen App Shope - Naive Bayes")
+
+user_input = st.text_area("Masukkan teks ulasan:")
+
+if st.button("Prediksi"):
+    if user_input.strip():
+        # Ubah teks jadi fitur
+        X = vectorizer.transform([user_input])
+        # Prediksi
+        prediction = model.predict(X)[0]
+
+        if prediction == 1:
+            st.success("Sentimen POSITIF 😀")
+        else:
+            st.error("Sentimen NEGATIF 😡")
+    else:
+        st.warning("Tolong masukkan teks terlebih dahulu.")
